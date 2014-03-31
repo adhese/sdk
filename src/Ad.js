5987f129791d13815a37a66fd3f94d86a275dba6
tim <tim@adhese.com>
2014-03-31 16:25:39 +0200

    preparing build / compress options

---
+++ src/Ad.js
@@ -0,0 +1,19 @@
+/**
+ * @class
+ * Ad object represents an internal advertisment that can be requested from the server.
+ * Both request and response are defined by this object. An ad can be found in the mainObject.ads array, through it's format code.
+ *
+ * Options can be passed to detemrine how the ad will behave:
+ * write: true/false, if true, Ad will immedeatily be written to the document via document.write when .tag is called
+ * 
+ * 
+ * @formatCode {string} The format code for this Ad, will be used as id in further reference to this Ad.
+ * @options	{object}	An object containing the available options for this Ad.
+ * @return {void}
+ */
+ Adhese.prototype.Ad = function(adhese, formatCode, options) {
+ 	var defaults = { write:false };
+ 	this.format = formatCode;
+ 	this.options = adhese.helper.merge(defaults, options);
+ 	return this;
+ };
