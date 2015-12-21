251535f074f2c38731bcdb958a0e1cc423174ab8
tim <tim@adhese.com>
2015-12-21 13:08:02 +0100

    implemented new rubicon multi user sync

--- src/rtb/RubiconUserSync.js
+++ src/rtb/RubiconUserSync.js
@@ -6,10 +6,8 @@
 Adhese.prototype.rubiconUserSync = function(option) {
 	// if no account given, do nothing
 	if (option && option.rp_account && option.rp_account!='') {
-		// if no cookies has been set yet
 		if(document.cookie.indexOf("rubicon_uid_last_sync")==-1) {
-			// do request to Rubicon, ignore response, next requests to .adhese.com will have rubicon_uid cookie
-			new Image().src = "https://pixel-eu.rubiconproject.com/exchange/sync.php?p=" + option.rp_account;
+			this.helper.addEvent("load", this.rubiconMultiSync, option.rp_account);
 			var date = new Date();
 			date.setDate(date.getDate()+1);
 			date.setHours(0);
@@ -17,6 +15,32 @@ Adhese.prototype.rubiconUserSync = function(option) {
 			date.setSeconds(0);
 			var diff = date.getTime() - new Date().getTime();
 			this.helper.createCookie("rubicon_uid_last_sync", diff, (diff/24/60/60/1000));
+			// also create domain cookie, so do a request to an .adhese.com endpoint with the current domain as qs param
+			// this endpoint wil create a cookie on .adhese.com containing the domain as passed
 		}
 	}
 };
+
+/**
+ * Multi Sync implementation of Rubicon User Sync
+ * 
+ * We request a pixel on .adhese.com that sets a cookie containing the domain that is being used.
+ * We send a user sync request to Rubicon that eventually is redirected to our .adhese.com user sync.
+ * That checks if there is a domain cookie, if so, redirect to the user sync on that domain, with rubicon_uid and domain as query string parameters
+ * This final endpoint will set the rubicon_uid as a cookie on the passed domain
+ * 
+ * @param  {string} rp_account identifier of the partner as set up by Rubicon
+ * @return {void}            
+ */
+Adhese.prototype.rubiconMultiSync = function(rp_account) {
+	var script = document.createElement("SCRIPT");
+	script.type = "text\/javascript";
+	script.setAttribute("data-partner",rp_account);
+	script.setAttribute("data-region","eu");
+	script.setAttribute("data-country","be");
+	script.setAttribute("data-endpoint","eu");
+	script.src = "http://assets.rubiconproject.com/utils/xapi/multi-sync.js";	
+	document.body.appendChild(script);
+}
+
+
