a6567b71bdea8b4b63a84b809958e951931fd0fd
Ben <ben@adhese.com>
2016-03-11 12:17:28 +0100

    check if inviewtracker is provided before calling it

--- src/visible/Visible.js
+++ src/visible/Visible.js
@@ -1,4 +1,6 @@
-window.AdheseVisibleData = [];
+if(!window.AdheseVisibleData){
+    window.AdheseVisibleData = [];
+}
 
 Adhese.prototype.checkVisible = function() {
     var that = this;
@@ -12,13 +14,18 @@ Adhese.prototype.checkVisible = function() {
             var rect = el.getBoundingClientRect();
             ads[i].visible = rect.height > 0 && rect.width > 0 && rect.top >= 0 && rect.left >= 0 && rect.bottom - rect.height * .5 <= (window.innerHeight || document.documentElement.clientHeight) && rect.right - rect.width * .5 <= (window.innerWidth || document.documentElement.clientWidth);
             if (ads[i].visible && !ads[i].active && !ads[i].tracked) {
-                that.track(ads[i].inviewTracker);
+                if(that.track(ads[i].inviewTracker){ // check if there is an inviewTracker
+                    that.track(ads[i].inviewTracker);
+                }
                 ads[i].active = true;
                 ads[i].out = setTimeout(function(activeAd) {
-                    that.track(activeAd.visibleTracker);
-                    activeAd.tracked = true;
-                    window.AdheseVisibleData.splice(window.AdheseVisibleData.indexOf(activeAd), 1);
-                }, 1e3, ads[i]);
+                        that.track(activeAd.visibleTracker);
+                        activeAd.tracked = true;
+                        window.AdheseVisibleData.splice(window.AdheseVisibleData.indexOf(activeAd), 1);
+                    },
+                    1000, // 1 second
+                    ads[i] //activeAd param
+                );
             } else if (!ads[i].visible && ads[i].active) {
                 clearTimeout(ads[i].out);
                 ads[i].active = false;
