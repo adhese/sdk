64a5254439c2b18db523f5f294ff5536a9944e39
Ben <ben@adhese.com>
2016-03-11 12:23:06 +0100

    check if inviewtracker is provided before calling it, fixed an error

--- src/visible/Visible.js
+++ src/visible/Visible.js
@@ -14,7 +14,7 @@ Adhese.prototype.checkVisible = function() {
             var rect = el.getBoundingClientRect();
             ads[i].visible = rect.height > 0 && rect.width > 0 && rect.top >= 0 && rect.left >= 0 && rect.bottom - rect.height * .5 <= (window.innerHeight || document.documentElement.clientHeight) && rect.right - rect.width * .5 <= (window.innerWidth || document.documentElement.clientWidth);
             if (ads[i].visible && !ads[i].active && !ads[i].tracked) {
-                if(that.track(ads[i].inviewTracker){ // check if there is an inviewTracker
+                if(ads[i].inviewTracker){ // check if there is an inviewTracker
                     that.track(ads[i].inviewTracker);
                 }
                 ads[i].active = true;
