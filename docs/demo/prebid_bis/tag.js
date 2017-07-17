3e023e51a52deacdff5e327eb807ae7dea68dbd4
Tim Sturtewagen <tim@adhese.com>
2017-07-17 16:43:11 +0200

    Some debug output added

--- docs/demo/prebid_bis/tag.js
+++ docs/demo/prebid_bis/tag.js
@@ -259,7 +259,10 @@ Adhese.prototype.getLocation = function() {
     var l = window.location.href != "about:blank" ? window.location : parent.window.location;
     var p = l.hostname.replace(new RegExp("www."), "") || "unknown";
     var s = l.pathname.replace(/^\/([^\/]*).*$/, "$1");
-    if (l.hostname.indexOf("pebblemedia.be") != -1 || l.hostname == "" || l.hostname == "adhese.github.io") {
+    if (l.hostname.indexOf("pebblemedia.be") != -1) {
+        s = "";
+        p = "sporza_socio_home";
+    } else if (l.hostname == "") {
         s = "";
         p = "flair-nl_others";
     }
@@ -1947,14 +1950,6 @@ if (typeof jQuery != "undefined" && typeof $ != "undefined") {
     });
 }
 
-var adhese = new Adhese();
-
-adhese.init();
-
-adhese.helper = {};
-
-adhese.helper.log = function() {};
-
 function getAdheseAdLoc() {
     var adheseLocation = "";
     if (adheseLocation == "" || adheseLocation == "unknown") {
@@ -1972,6 +1967,195 @@ function getAdheseAdLoc() {
         if (~window.location.host.indexOf("meteo1")) adheseLocation = "_" + adheseSite + "_meteo_";
     }
     return adheseLocation;
+}
+
+if (!window.AdheseVisibleData) {
+    window.AdheseVisibleData = [];
+}
+
+Adhese.prototype.checkVisible = function() {
+    var that = this;
+    var ads = new Array();
+    var visibleIndex = 0;
+    for (var i = 0; i < window.AdheseVisibleData.length; i++) {
+        visibleIndex = i;
+        ads[i] = window.AdheseVisibleData[i];
+        var el = document.getElementById(ads[i].uid);
+        if (el) {
+            var rect = el.getBoundingClientRect();
+            ads[i].visible = rect.height > 0 && rect.width > 0 && rect.top + rect.height * .5 >= 0 && rect.left + rect.width * .5 >= 0 && rect.bottom - rect.height * .5 <= (window.innerHeight || document.documentElement.clientHeight) && rect.right - rect.width * .5 <= (window.innerWidth || document.documentElement.clientWidth);
+            if (ads[i].visible && !ads[i].active && !ads[i].tracked) {
+                if (ads[i].inviewTracker) {
+                    that.track(ads[i].inviewTracker);
+                }
+                ads[i].active = true;
+                ads[i].out = setTimeout(function(activeAd) {
+                    that.track(activeAd.visibleTracker);
+                    activeAd.tracked = true;
+                    window.AdheseVisibleData.splice(window.AdheseVisibleData.indexOf(activeAd), 1);
+                }, 1e3, ads[i]);
+            } else if (!ads[i].visible && ads[i].active) {
+                clearTimeout(ads[i].out);
+                ads[i].active = false;
+            }
+        } else {
+            this.helper.log("Can't find <div> width id: " + ads[i].uid);
+            ads[i].tracked = true;
+        }
+    }
+};
+
+var AdheseAjax = {
+    request: function(ops) {
+        if (typeof ops == "string") ops = {
+            url: ops
+        };
+        ops.url = ops.url || "";
+        ops.method = ops.method || "get";
+        ops.data = ops.data || {};
+        if (typeof ops.encodeData == "undefined") {
+            ops.encodeData = true;
+        }
+        var getParams = function(data, url) {
+            var arr = [], str;
+            for (var name in data) {
+                arr.push(name + "=" + encodeURIComponent(data[name]));
+            }
+            str = arr.join("&");
+            if (str != "") {
+                return url ? url.indexOf("?") < 0 ? "?" + str : "&" + str : str;
+            }
+            return "";
+        };
+        var api = {
+            host: {},
+            process: function(ops) {
+                var self = this;
+                this.xhr = null;
+                if (document.all && !window.atob) {
+                    try {
+                        this.xhr = new ActiveXObject("Msxml2.XMLHTTP");
+                    } catch (e) {
+                        try {
+                            this.xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                        } catch (e) {
+                            this.xhr = false;
+                        }
+                    }
+                } else {
+                    try {
+                        this.xhr = new XMLHttpRequest();
+                    } catch (e) {
+                        this.xhr = false;
+                    }
+                }
+                if (this.xhr) {
+                    if ("withCredentials" in this.xhr) {
+                        this.xhr.withCredentials = true;
+                    }
+                    this.xhr.onreadystatechange = function() {
+                        if (self.xhr.readyState == 4 && self.xhr.status == 200) {
+                            var result = self.xhr.responseText;
+                            if (ops.json === true && typeof JSON != "undefined") {
+                                if (result) {
+                                    try {
+                                        result = JSON.parse(result);
+                                        self.doneCallback && self.doneCallback.apply(self.host, [ result, self.xhr ]);
+                                    } catch (e) {
+                                        self.errorCallback && self.errorCallback.apply(self.host, [ "Adhese Ajax: " + e ]);
+                                    }
+                                } else {
+                                    self.errorCallback && self.errorCallback.apply(self.host, [ "Adhese Ajax: Response is empty string" ]);
+                                }
+                            }
+                        } else if (self.xhr.readyState == 4) {
+                            self.failCallback && self.failCallback.apply(self.host, [ self.xhr ]);
+                        }
+                        self.alwaysCallback && self.alwaysCallback.apply(self.host, [ self.xhr ]);
+                    };
+                }
+                if (ops.method == "get") {
+                    this.xhr.open("GET", ops.url + getParams(ops.data, ops.url), true);
+                } else {
+                    this.xhr.open(ops.method, ops.url, true);
+                    this.setHeaders({
+                        "X-Requested-With": "XMLHttpRequest",
+                        "Content-type": "application/x-www-form-urlencoded"
+                    });
+                }
+                if (ops.headers && typeof ops.headers == "object") {
+                    this.setHeaders(ops.headers);
+                }
+                setTimeout(function() {
+                    if (ops.method == "get") {
+                        self.xhr.send();
+                    } else {
+                        var data;
+                        if (ops.encodeData) {
+                            data = getParams(ops.data);
+                        } else {
+                            data = ops.data;
+                        }
+                        self.xhr.send(data);
+                    }
+                }, 20);
+                return this;
+            },
+            done: function(callback) {
+                this.doneCallback = callback;
+                return this;
+            },
+            fail: function(callback) {
+                this.failCallback = callback;
+                return this;
+            },
+            always: function(callback) {
+                this.alwaysCallback = callback;
+                return this;
+            },
+            error: function(callback) {
+                this.errorCallback = callback;
+                return this;
+            },
+            setHeaders: function(headers) {
+                for (var name in headers) {
+                    this.xhr && this.xhr.setRequestHeader(name, headers[name]);
+                }
+            }
+        };
+        return api.process(ops);
+    }
+};
+
+Adhese.prototype.track = function(uri) {
+    var img = document.createElement("img");
+    img.src = uri;
+    img.style.height = "1px";
+    img.style.width = "1px";
+    img.style.margin = "-1px";
+    img.style.border = "0";
+    img.style.position = "absolute";
+    img.style.top = "0";
+    document.body.appendChild(img);
+};
+
+var adhese = new Adhese();
+
+adhese.init();
+
+adhese.helper = {};
+
+adhese.helper.log = function() {};
+
+adhese.addEvent("load", adhese.checkVisible.bind(adhese));
+
+adhese.addEvent("scroll", adhese.checkVisible.bind(adhese));
+
+if (window.location.hostname == "") {
+    var prebidScript = document.createElement("script");
+    prebidScript.src = "prebid.js";
+    prebidScript.onload = function() {};
+    document.head.appendChild(prebidScript);
 }
 
 var adh_interests = adhese.profile["in"].split(";");
@@ -2191,182 +2375,3 @@ if (pbmCxCkSegments != "") {
 } else {
     adhese.sgArray.push("nocX");
 }
-
-if (!window.AdheseVisibleData) {
-    window.AdheseVisibleData = [];
-}
-
-Adhese.prototype.checkVisible = function() {
-    var that = this;
-    var ads = new Array();
-    var visibleIndex = 0;
-    for (var i = 0; i < window.AdheseVisibleData.length; i++) {
-        visibleIndex = i;
-        ads[i] = window.AdheseVisibleData[i];
-        var el = document.getElementById(ads[i].uid);
-        if (el) {
-            var rect = el.getBoundingClientRect();
-            ads[i].visible = rect.height > 0 && rect.width > 0 && rect.top + rect.height * .5 >= 0 && rect.left + rect.width * .5 >= 0 && rect.bottom - rect.height * .5 <= (window.innerHeight || document.documentElement.clientHeight) && rect.right - rect.width * .5 <= (window.innerWidth || document.documentElement.clientWidth);
-            if (ads[i].visible && !ads[i].active && !ads[i].tracked) {
-                if (ads[i].inviewTracker) {
-                    that.track(ads[i].inviewTracker);
-                }
-                ads[i].active = true;
-                ads[i].out = setTimeout(function(activeAd) {
-                    that.track(activeAd.visibleTracker);
-                    activeAd.tracked = true;
-                    window.AdheseVisibleData.splice(window.AdheseVisibleData.indexOf(activeAd), 1);
-                }, 1e3, ads[i]);
-            } else if (!ads[i].visible && ads[i].active) {
-                clearTimeout(ads[i].out);
-                ads[i].active = false;
-            }
-        } else {
-            this.helper.log("Can't find <div> width id: " + ads[i].uid);
-            ads[i].tracked = true;
-        }
-    }
-};
-
-var AdheseAjax = {
-    request: function(ops) {
-        if (typeof ops == "string") ops = {
-            url: ops
-        };
-        ops.url = ops.url || "";
-        ops.method = ops.method || "get";
-        ops.data = ops.data || {};
-        if (typeof ops.encodeData == "undefined") {
-            ops.encodeData = true;
-        }
-        var getParams = function(data, url) {
-            var arr = [], str;
-            for (var name in data) {
-                arr.push(name + "=" + encodeURIComponent(data[name]));
-            }
-            str = arr.join("&");
-            if (str != "") {
-                return url ? url.indexOf("?") < 0 ? "?" + str : "&" + str : str;
-            }
-            return "";
-        };
-        var api = {
-            host: {},
-            process: function(ops) {
-                var self = this;
-                this.xhr = null;
-                if (document.all && !window.atob) {
-                    try {
-                        this.xhr = new ActiveXObject("Msxml2.XMLHTTP");
-                    } catch (e) {
-                        try {
-                            this.xhr = new ActiveXObject("Microsoft.XMLHTTP");
-                        } catch (e) {
-                            this.xhr = false;
-                        }
-                    }
-                } else {
-                    try {
-                        this.xhr = new XMLHttpRequest();
-                    } catch (e) {
-                        this.xhr = false;
-                    }
-                }
-                if (this.xhr) {
-                    if ("withCredentials" in this.xhr) {
-                        this.xhr.withCredentials = true;
-                    }
-                    this.xhr.onreadystatechange = function() {
-                        if (self.xhr.readyState == 4 && self.xhr.status == 200) {
-                            var result = self.xhr.responseText;
-                            if (ops.json === true && typeof JSON != "undefined") {
-                                if (result) {
-                                    try {
-                                        result = JSON.parse(result);
-                                        self.doneCallback && self.doneCallback.apply(self.host, [ result, self.xhr ]);
-                                    } catch (e) {
-                                        self.errorCallback && self.errorCallback.apply(self.host, [ "Adhese Ajax: " + e ]);
-                                    }
-                                } else {
-                                    self.errorCallback && self.errorCallback.apply(self.host, [ "Adhese Ajax: Response is empty string" ]);
-                                }
-                            }
-                        } else if (self.xhr.readyState == 4) {
-                            self.failCallback && self.failCallback.apply(self.host, [ self.xhr ]);
-                        }
-                        self.alwaysCallback && self.alwaysCallback.apply(self.host, [ self.xhr ]);
-                    };
-                }
-                if (ops.method == "get") {
-                    this.xhr.open("GET", ops.url + getParams(ops.data, ops.url), true);
-                } else {
-                    this.xhr.open(ops.method, ops.url, true);
-                    this.setHeaders({
-                        "X-Requested-With": "XMLHttpRequest",
-                        "Content-type": "application/x-www-form-urlencoded"
-                    });
-                }
-                if (ops.headers && typeof ops.headers == "object") {
-                    this.setHeaders(ops.headers);
-                }
-                setTimeout(function() {
-                    if (ops.method == "get") {
-                        self.xhr.send();
-                    } else {
-                        var data;
-                        if (ops.encodeData) {
-                            data = getParams(ops.data);
-                        } else {
-                            data = ops.data;
-                        }
-                        self.xhr.send(data);
-                    }
-                }, 20);
-                return this;
-            },
-            done: function(callback) {
-                this.doneCallback = callback;
-                return this;
-            },
-            fail: function(callback) {
-                this.failCallback = callback;
-                return this;
-            },
-            always: function(callback) {
-                this.alwaysCallback = callback;
-                return this;
-            },
-            error: function(callback) {
-                this.errorCallback = callback;
-                return this;
-            },
-            setHeaders: function(headers) {
-                for (var name in headers) {
-                    this.xhr && this.xhr.setRequestHeader(name, headers[name]);
-                }
-            }
-        };
-        return api.process(ops);
-    }
-};
-
-Adhese.prototype.track = function(uri) {
-    var img = document.createElement("img");
-    img.src = uri;
-    img.style.height = "1px";
-    img.style.width = "1px";
-    img.style.margin = "-1px";
-    img.style.border = "0";
-    img.style.position = "absolute";
-    img.style.top = "0";
-    document.body.appendChild(img);
-};
-
-adhese.addEvent("load", adhese.checkVisible.bind(adhese));
-
-adhese.addEvent("scroll", adhese.checkVisible.bind(adhese));
-
-var prebidScript = document.createElement("script");
-prebidScript.src = "prebid.js";
-prebidScript.onload = function() {};
-document.head.appendChild(prebidScript);
